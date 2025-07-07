7b8aa17b03e29d35853529f7ada276d8eb5e5b0a
Fox <thefoxjones@gmail.com>
2025-07-07 17:18:38 -0500

    is-true 1.31.

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-var objectPropIsTrue = require('is-true')
-var isNotTrue        = require('is-not-true')
-
-module.exports = function (thing) {
-  try {
-    return !isNotTrue(thing)
-  } catch {
-    return objectPropIsTrue({ thing }, 'thing')
-  }
+module.exports = function (value) {
+  return value === true
 }
